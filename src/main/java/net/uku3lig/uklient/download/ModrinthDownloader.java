4dd4a21eb61a629a0852746205f22eb7f42d217e
Uku3lig <thelololomail@gmail.com>
2021-12-12 22:16:33 +0100

    remove shitty unused code

--- src/main/java/net/uku3lig/uklient/download/ModrinthDownloader.java
+++ src/main/java/net/uku3lig/uklient/download/ModrinthDownloader.java
@@ -1,32 +1,20 @@
 package net.uku3lig.uklient.download;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import net.uku3lig.uklient.model.ModrinthFile;
 import net.uku3lig.uklient.util.Util;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 public class ModrinthDownloader {
-    private static final Gson gson = new Gson();
-    private static final Map<String, String> mods = new HashMap<>();
-
     private static final String BASE_URL = "https://api.modrinth.com/api/v1/";
     private static final ModrinthRequester requester = RequestManager.supplyRetrofit(BASE_URL).create(ModrinthRequester.class);
-
-    public static String getModID(String name) {
-        if (mods.isEmpty()) loadMods();
-        return mods.get(name);
-    }
 
     public static CompletableFuture<URL> getMostRecentFile(String modId, String mcVer) {
         return requester.getFiles(modId).thenApply(l -> l.stream()
@@ -47,21 +35,6 @@ public class ModrinthDownloader {
         if (!destFolder.isDirectory())
             throw new IllegalArgumentException(destFolder.getAbsolutePath() + " is not a folder!!!");
         return getMostRecentFile(modId, mcVer).thenCompose(url -> Downloader.download(url, Util.path(url, destFolder)));
-    }
-
-    private static void loadMods() {
-        InputStream in = ModrinthDownloader.class.getClassLoader().getResourceAsStream("mods.json");
-        if (in == null) throw new NullPointerException("Error: could not find file mods.json");
-
-        try (InputStreamReader reader = new InputStreamReader(in)) {
-            JsonObject root = gson.fromJson(reader, JsonObject.class);
-            Map<String, String> map = root.entrySet().stream()
-                    .collect(Collectors.toMap(Map.Entry::getKey, v -> v.getValue().getAsString()));
-
-            mods.putAll(map);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     private ModrinthDownloader() {
