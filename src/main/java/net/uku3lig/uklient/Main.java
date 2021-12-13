3aa06efa998945fde6a9789f631fdcd425f957f0
Uku3lig <thelololomail@gmail.com>
2021-12-13 20:29:23 +0100

    added main class

---
+++ src/main/java/net/uku3lig/uklient/Main.java
@@ -0,0 +1,17 @@
+package net.uku3lig.uklient;
+
+public class Main {
+    public static final String COW =
+            " ___________________________ \n" +
+            "< uklient installer poggers >\n" +
+            " --------------------------- \n" +
+            "        \\   ^__^\n" +
+            "         \\  (oo)\\_______\n" +
+            "            (__)\\       )\\/\\\n" +
+            "                ||----w |\n" +
+            "                ||     ||";
+
+    public static void main(String[] args) {
+        System.out.println(COW);
+    }
+}
