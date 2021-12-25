3e59986c2cc0f11ca0b16bccfe4d7b7f17ee4bb0
Uku3lig <thelololomail@gmail.com>
2021-12-25 21:52:32 +0100

    added a check for typos in presets

--- src/main/java/net/uku3lig/uklient/model/NamedModList.java
+++ src/main/java/net/uku3lig/uklient/model/NamedModList.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import net.uku3lig.uklient.download.ResourceManager;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Getter
@@ -14,6 +15,18 @@ public class NamedModList {
     private List<String> mods;
 
     public List<ModInfo> getModInfos() {
+        if (!ResourceManager.getMods().stream().allMatch(m -> mods.contains(m.getName()))) {
+            mods.stream()
+                    .filter(s -> {
+                        try {
+                            ResourceManager.getModFromName(s);
+                            return false;
+                        } catch (NoSuchElementException e) {
+                            return true;
+                        }
+                    }).forEach(s -> System.err.printf("\rmod %s does not exist in preset %s!%n", s, name));
+        }
+
         return ResourceManager.getMods().stream()
                 .filter(m -> mods.contains(m.getName()))
                 .collect(Collectors.toList());
