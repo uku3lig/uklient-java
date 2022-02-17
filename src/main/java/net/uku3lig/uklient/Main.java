a2f36b1493c72dd4c4143b25453e238ef17faf16
uku <thelololomail@gmail.com>
2022-02-17 19:29:13 +0100

    cleaned up main class

--- src/main/java/net/uku3lig/uklient/Main.java
+++ src/main/java/net/uku3lig/uklient/Main.java
@@ -1,13 +1,12 @@
 package net.uku3lig.uklient;
 
 import com.diogonunes.jcolor.Attribute;
+import lombok.Getter;
 import me.tongfei.progressbar.ProgressBar;
-import net.uku3lig.uklient.download.CurseforgeDownloader;
 import net.uku3lig.uklient.download.FabricInstaller;
-import net.uku3lig.uklient.download.ModrinthDownloader;
-import net.uku3lig.uklient.util.*;
 import net.uku3lig.uklient.model.ModInfo;
 import net.uku3lig.uklient.model.ModList;
+import net.uku3lig.uklient.util.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -22,50 +21,34 @@ import java.util.stream.Collectors;
 public class Main {
     public static final String COW =
             " ___________________________ \n" +
-            "< &luklient installer poggers&r >\n" +
-            " --------------------------- \n" +
-            "        \\   ^__^\n" +
-            "         \\  (oo)\\_______\n" +
-            "            (__)\\       )\\/\\\n" +
-            "                ||----w |\n" +
-            "                ||     ||";
+                    "< &luklient installer poggers&r >\n" +
+                    " --------------------------- \n" +
+                    "        \\   ^__^\n" +
+                    "         \\  (oo)\\_______\n" +
+                    "            (__)\\       )\\/\\\n" +
+                    "                ||----w |\n" +
+                    "                ||     ||";
 
-    static final List<String> AVAILABLE_MC_VERSIONS = Arrays.asList("1.18.1", "1.17.1", "1.16.5");
+    @Getter // FIXME put versions in a file in uklient-resources
+    static final List<String> versions = Arrays.asList("1.18.1", "1.17.1", "1.16.5");
     public static final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public static void main(String[] args) {
         System.out.println(Color.parse(COW, Attribute.BOLD()));
 
-        Path mcPath = MinecraftHelper.findMcDir();
-        while (!Files.isDirectory(mcPath) || !Files.exists(mcPath.resolve("launcher_profiles.json"))) {
-            System.out.println("Cannot find your .minecraft directory");
-            mcPath = Paths.get(UIManager.input("Please input your .minecraft directory path")).normalize().toAbsolutePath();
-        }
+        Path mcPath = getMcPath();
+        boolean createProfile = Files.exists(mcPath.resolve("launcher_profiles.json"));
 
-        System.out.println("\n\n");
-        System.out.println(Color.parse("&3Please choose a Minecraft version:", Attribute.BOLD()));
-        final String mcVer = AVAILABLE_MC_VERSIONS.get(UIManager.choice(AVAILABLE_MC_VERSIONS));
+        Path installDir = getInstallDir(mcPath);
 
-        System.out.println(Color.parse("\n\n&3Please choose an installation directory:", Attribute.BOLD()));
-        List<String> choices = Arrays.asList("Install in .uklient (recommended)",
-                "Install in .minecraft (will delete all your current mods)", "Install in another directory");
-        Path installDir;
-        switch (UIManager.choice(choices, 0)) {
-            case 0:
-                installDir = mcPath.getParent().resolve(".uklient");
-                break;
-            case 1:
-                installDir = mcPath;
-                break;
-            default:
-                do {
-                    installDir = Paths.get(UIManager.input("Please input your installation directory")).normalize().toAbsolutePath();
-                } while (Files.exists(installDir) && !Files.isDirectory(installDir));
-        }
+        System.out.println(Color.parse("\n\n&3Please choose a Minecraft version:", Attribute.BOLD()));
+        final String mcVer = versions.get(UIManager.choice(versions));
 
-        System.out.println(Color.parse("\n\n&3Please choose a preset:", Attribute.BOLD()));
-        LinkedHashMap<String, String> presetNames = new LinkedHashMap<>(ResourceManager.getPresets().stream()
-                .collect(Collectors.toMap(ModList::getDisplayName, ModList::getName)));
+        System.out.println("\n\nDownloading mods and presets info...");
+        List<ModList> presets = ResourceManager.getPresets();
+
+        System.out.println(Color.parse("&3Please choose a preset:", Attribute.BOLD()));
+        LinkedHashMap<String, String> presetNames = new LinkedHashMap<>(presets.stream().collect(Collectors.toMap(ModList::getDisplayName, ModList::getName)));
         List<String> displayNames = new LinkedList<>(presetNames.keySet());
 
         ModList preset = ResourceManager.getPresetByName(presetNames.get(displayNames.get(UIManager.choice(displayNames, 0))));
@@ -100,7 +83,6 @@ public class Main {
         final Path finalMcPath = mcPath;
         final Path finalInstallDir = installDir;
 
-
         FabricInstaller.installFabric(mcVer, mcPath, executor)
                 .thenCompose(v -> {
                     final ProgressBar pb = Util.getProgressBar("Downloading Mods", mods.size());
@@ -112,10 +94,45 @@ public class Main {
                 .thenRun(() -> mods.forEach(m -> m.copyConfig(preset.getName(), configPath)))
                 .thenRun(() -> System.out.println("Copied all config files"))
                 .thenCompose(v -> FabricInstaller.getLatestFabricLoader())
-                .thenAccept(f -> MinecraftHelper.createLauncherProfile(finalMcPath, finalInstallDir, f, mcVer))
+                .thenAccept(f -> {
+                    if (createProfile) MinecraftHelper.createLauncherProfile(finalMcPath, finalInstallDir, f, mcVer);
+                })
                 .thenRun(() -> System.out.println("Created launcher profile"))
                 .thenRun(() -> MinecraftHelper.copyUserFiles(finalMcPath, finalInstallDir))
                 .thenRun(() -> System.out.println("Copied resource packs and options"))
                 .thenRun(() -> System.out.println(Color.parse("\n&3uklient installed! you can now close this window", Attribute.BOLD())));
+    }
+
+    private static Path getMcPath() {
+        Path mcPath = MinecraftHelper.findMcDir();
+        while (mcPath == null || !Files.isDirectory(mcPath)) {
+            System.out.println("Cannot find your .minecraft directory");
+            mcPath = Paths.get(UIManager.input("Please input your .minecraft directory path")).normalize().toAbsolutePath();
+            if (!Files.exists(mcPath.resolve("launcher_profiles.json"))) {
+                System.out.println("This directory does not appear to be your .minecraft folder");
+                if (!UIManager.yesNo("Are you sure you want to proceed?", false)) mcPath = null;
+            }
+        }
+        return mcPath;
+    }
+
+    private static Path getInstallDir(Path mcPath) {
+        System.out.println(Color.parse("\n\n&3Please choose an installation directory:", Attribute.BOLD()));
+        List<String> choices = Arrays.asList("Install in .uklient (recommended)",
+                "Install in .minecraft (will delete all your current mods)", "Install in another directory");
+
+        switch (UIManager.choice(choices, 0)) {
+            case 0:
+                return mcPath.getParent().resolve(".uklient");
+            case 1:
+                return mcPath;
+            default:
+                Path installDir = Paths.get(UIManager.input("Please input your installation directory")).normalize().toAbsolutePath();
+                while (Files.exists(installDir) && !Files.isDirectory(installDir)) {
+                    System.out.println("This directory does not appear to exist");
+                    installDir = Paths.get(UIManager.input("Please input your installation directory")).normalize().toAbsolutePath();
+                }
+                return installDir;
+        }
     }
 }
